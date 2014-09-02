5bfb40675a6a39b6ef8827acc06576134902f3f8
Adriana Pineda <am.pineda206@gmail.com>
2014-09-01 20:45:11 -0500

    Remove files that are in .gitignore

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = '03c30751db55d6fa98265f7bc98f8f6da29347d2ba215de90484afb308904a1465d6a6a866adcccf6bbbe0eae1ffb366029b901d97fa4521bc351b67025fc853'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
