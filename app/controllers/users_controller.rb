76e34bca959c13a88e02c608169c75e6532f16aa
Adriana Pineda <am.pineda206@gmail.com>
2014-09-13 09:00:08 -0500

    Chapter 9 exercises

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: [:destroy]
-  
+  before_action :not_signed_in_user, only: [:new, :create]
+
   def new
     @user = User.new
   end
@@ -15,9 +16,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    unless current_user?(@user)
     User.find(params[:id]).destroy
     flash[:success] = "User deleted."
     redirect_to users_url
+    end
   end
 
   def index
@@ -58,6 +61,12 @@ class UsersController < ApplicationController
       unless signed_in?
         store_location
         redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def not_signed_in_user
+      unless !signed_in?
+        redirect_to root_url
       end
     end
 
